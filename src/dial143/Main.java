a2579e8d4f71813cc62deafc8c4c7cb9c42c1d67
renmaya <whatfu49@gmail.com>
2026-03-10 01:23:44 +0800

    created GUI DialoguePanel,MainFrame,SettingsPanel,TitleScreen

--- src/dial143/Main.java
+++ src/dial143/Main.java
@@ -1,5 +1,8 @@
 package dial143;
 
+import GUI.panels.MainFrame;
+import java.awt.EventQueue;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -10,9 +13,26 @@ package dial143;
  * @author renmaya
  */
 public class Main {
-    public static void main(String[] args) {
-        
-        System.out.println("New Main");
-    }
+public static void main(String args[]) {
+    EventQueue.invokeLater(new Runnable() {
+        public void run() {
+            MainFrame frame = new MainFrame();
+            
+            // Ensure frame has size
+            frame.pack();            
+            java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+            java.awt.Dimension frameSize = frame.getSize();
+            
+            // Calculate center position
+            int x = (screenSize.width - frameSize.width) / 2;
+            int y = (screenSize.height - frameSize.height) / 2;
+            
+            // Force position
+            frame.setLocation(x, y);
+            frame.setVisible(true);
+            
+        }
+    });
+}
     
 }
